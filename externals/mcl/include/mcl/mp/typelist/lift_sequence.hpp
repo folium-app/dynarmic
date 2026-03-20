51a74c4f0bd7bed60b268d0e37e442e9e774c3ec
Jarrod Norwell <official.antique@gmail.com>
2026-03-21 01:33:00 +0800

    Fixed Clang 21 error

--- externals/mcl/include/mcl/mp/typelist/lift_sequence.hpp
+++ externals/mcl/include/mcl/mp/typelist/lift_sequence.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <type_traits>
+#include <utility>
 
 #include "mcl/mp/typelist/list.hpp"
 
@@ -15,15 +16,21 @@ namespace detail {
 template<class VL>
 struct lift_sequence_impl;
 
+// std::integer_sequence specialization
+template<class T, T... values>
+struct lift_sequence_impl<std::integer_sequence<T, values...>> {
+    using type = list<std::integral_constant<T, values>...>;
+};
+
+// generic value-list (optional)
 template<class T, template<class, T...> class VLT, T... values>
 struct lift_sequence_impl<VLT<T, values...>> {
     using type = list<std::integral_constant<T, values>...>;
 };
 
-}  // namespace detail
+} // namespace detail
 
-/// Lifts values in value list VL to create a type list.
 template<class VL>
 using lift_sequence = typename detail::lift_sequence_impl<VL>::type;
 
-}  // namespace mcl::mp
+} // namespace mcl::mp
